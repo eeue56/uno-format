27891cf1d53d0970bc0d253193b17146a596240a
Noah <eeue56@users.noreply.github.com>
2017-03-01 16:59:15 +0100

    autobracer

--- formatter.py
+++ formatter.py
@@ -6,12 +6,32 @@ def format_file(file):
 	end_pattern = re.compile("\)[ \t]+\{")
 	pattern = re.compile('([ \t]+)(.*\)[ \t]+)\{', flags=re.MULTILINE)
 
+
+	class_end_pattern = re.compile("[ \t]+\{")
+	class_pattern = re.compile('([ \t]+)(.*class.*[ \t]+)\{', flags=re.MULTILINE)
+
+	struct_end_pattern = re.compile("[ \t]+\{")
+	struct_pattern = re.compile('([ \t]+)(.*struct.*[ \t]+)\{', flags=re.MULTILINE)
+
 	with open(file) as f:
 		for line in f:
 			match = pattern.match(line)
-			if pattern.match(line):
+			if match:
 				indent_level =  match.groups()[0]
 				line = end_pattern.sub(")\n" + indent_level + "{", line)
+
+			match = class_pattern.match(line)
+			if match:
+				print(line)
+				indent_level =  match.groups()[0]
+				line = class_end_pattern.sub(")\n" + indent_level + "{", line)
+
+			match = struct_pattern.match(line)
+			if match:
+				print(line)
+				indent_level =  match.groups()[0]
+				line = struct_end_pattern.sub(")\n" + indent_level + "{", line)
+
 			lines.append(line)
 
 
@@ -20,5 +40,5 @@ def format_file(file):
 
 
 if __name__ == '__main__':
-  import sys
+	import sys
 	format_file(sys.argv[1])
